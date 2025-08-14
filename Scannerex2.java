f8bc6f78c45848b7090d8a901903a28ba19d0f27
Samiksha0612 <yogitaraut0311@gmail.com>
2025-08-13 18:35:16 -1200

    Scannerex.java

---
+++ Scannerex2.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.Scanner;
+public class Scannerex2{
+public static void main(String args[]){
+Scanner obj=new Scanner(System.in);
+System.out.println("enter a address");
+String address=obj.nextLine();
+System.out.println("your name is:" +address);
+}
+}
---
+++ Scannerex3.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.Scanner;
+public class Scannerex3{
+public static void main(String args[]){
+Scanner obj=new Scanner(System.in);
+System.out.println("enter a boolean");
+boolean data=obj.nextBoolean();
+System.out.println("your name is:" +data);
+}
+}
